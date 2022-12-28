2c387c6d4ba36e1d6341776d950e450f0cda767f
hs12 <77109954+hyun2371@users.noreply.github.com>
2022-12-28 21:36:41 +0900

    Section3: Test Memeber Service

--- hello-spring/src/main/java/service/MemberService.java
+++ hello-spring/src/main/java/service/MemberService.java
@@ -2,21 +2,23 @@ package service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //외부에서 받아옴 (Dependency Injection)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
      */
     public Long join(Member member){
-
-
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
@@ -26,7 +28,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                         .ifPresent(m->{
-                            throw new IllegalStateException("이미 존재하는 회원입니다");
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
                         });
     }
 
