b7386234357665a44f8a39e0904416da9cfd7ede
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-01-08 22:42:24 +0900

    Section5: 회원 웹 기능-조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 //어노테이션을 보고 객체를 생성하여 스프링 컨테이너에 담아둠
@@ -34,6 +37,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/"; //회원가입이 끝나면 홈 화면으로 보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
     }
 }
 
