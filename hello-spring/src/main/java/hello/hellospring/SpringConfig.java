a82dc09594436442c4a91ab62b334147a5deef6e
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-01-12 22:45:21 +0900

    Section7: AOP 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -19,14 +19,13 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository);
     }
 
-    //   @Bean
-//    public MemberRepository memberRepository(){
-//        //return new MemoryMemberRepository();
-//        //return new JdbcMemberRepository(dataSource);
-//        //return new JdbcTemplateMemberRepository(dataSource);
-//        //return new JpaMemberRepository(em);
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
 //    }
+
 }
